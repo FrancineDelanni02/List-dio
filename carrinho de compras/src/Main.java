64a6d2cdb3568bdd9e30c963ac89192eb3dbfd51
FrancineDelanni02 <delannifrancine@gmail.com>
2025-02-18 05:20:18 -0300

    update CarrinhoDeCompras e Main

--- carrinho de compras/src/Main.java
+++ carrinho de compras/src/Main.java
@@ -2,6 +2,19 @@ public class Main {
     public static void main(String[] args) {
         CarrinhoDeCompras c = new CarrinhoDeCompras();
 
-        c.adicionarItem("Banana",12,3);
+        c.adicionarItem("Banana",1,3);
+        c.adicionarItem("Uva",1,1);
+        c.adicionarItem("Fone de ouvido",1,1);
+        c.adicionarItem("Banana",1,1);
+        c.adicionarItem("Banana",1,1);
+        c.adicionarItem("Prato de porcelana",1,1);
+        c.adicionarItem("Torneira",1,1);
+        c.adicionarItem("Bandeira do Brasil",1,1);
+
+//        System.out.println(c.calcularValorTotal());
+//        c.exibirItens();
+//        c.removerItem("Banana");
+//        System.out.println("=============================================");
+//        c.exibirItens();
     }
 }
