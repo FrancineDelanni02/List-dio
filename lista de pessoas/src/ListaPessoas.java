1f4ee6f46dde72cbe7e0fe1689cf272b6461306e
FrancineDelanni02 <delannifrancine@gmail.com>
2025-02-18 05:49:04 -0300

    create list and update Pessoa

---
+++ lista de pessoas/src/ListaPessoas.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaPessoas {
+    private List<Pessoa> pessoas;
+
+    public ListaPessoas() {
+        pessoas = new ArrayList<Pessoa>();
+    }
+}
